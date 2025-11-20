f41bda0107a768424f562ac694c67d03159fdb07
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-20 14:42:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function lazyLoadImages() {
+  const lazyImages = document.querySelectorAll('img.lazy');
+
+  const observer = new IntersectionObserver((entries, obs) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+
+        img.src = img.dataset.src;
+
+        obs.unobserve(img);
+      }
+    });
+  });
+
+  lazyImages.forEach(img => observer.observe(img));
+}
+
+document.addEventListener("DOMContentLoaded", lazyLoadImages);
